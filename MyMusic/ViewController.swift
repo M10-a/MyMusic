9ca30a4756cd6d05940f96d807a3ce39449b6c48
浅野未央 <mio@asanomionoMacBook-Pro.local>
2017-05-20 16:04:16 +0900

    リファクタリング

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -12,50 +12,50 @@ import AVFoundation
 
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-//シンバルの音源ファイル指定
+  //シンバルの音源ファイル指定
   let cymbalpath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
- 
-//シンバルのプレイヤーインスタンス
+  
+  //シンバルのプレイヤーインスタンス
   var cymbalPiayer = AVAudioPlayer()
   //Bundle＝格納。変数でも一個しか使ってないとアラートくる
   
   
-  @IBAction func cymbal(_ sender: Any) {
+  @IBAction func cymba(_ sender: Any) {
     //_省略されている
-  do{
-    cymbalPiayer = try AVAudioPlayer(contentsOf: cymbalpath, fileTypeHint: nil)
-    cymbalPiayer.play()
-  } catch {
-    print("シンバルでエラーが発生しました！")
-    //AVAudioPlayer 例外が発生しやすいので、例外処理を記述しないといけない
+    do{
+      cymbalPiayer = try AVAudioPlayer(contentsOf: cymbalpath, fileTypeHint: nil)
+      cymbalPiayer.play()
+    } catch {
+      print("シンバルでエラーが発生しました！")
+      //AVAudioPlayer 例外が発生しやすいので、例外処理を記述しないといけない
     }
   }
   
-// ギター
+  // ギター
   let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
   
   var guitarPlayer = AVAudioPlayer()
   
   @IBAction func guitar(_ sender: Any) {
     do {
-    guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
-    guitarPlayer.play()
+      guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+      guitarPlayer.play()
     } catch {
       print("ギターでエラーが発生しました！")
     }
   }
   
-//バックミュージック
+  //バックミュージック
   let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
   
   var backmusicPlayer = AVAudioPlayer()
@@ -63,7 +63,7 @@ class ViewController: UIViewController {
   @IBAction func play(_ sender: Any) {
     do {
       backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
-  
+      
       //リピート設定（−１＝無限）
       backmusicPlayer.numberOfLoops = -1
       backmusicPlayer.play()
@@ -71,17 +71,25 @@ class ViewController: UIViewController {
       print("エラーが発生しました！")
     }
   }
- 
- //STOP
+  
+  //STOP
   @IBAction func stop(_ sender: Any) {
     backmusicPlayer.stop()
   }
+  
+  fileprivate func soundPlayer(_ player:inout AVAudioPlayer, path:URL, count: Int){
+    do{
+      player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+      player.numberOfLoops = count
+      player.play()
+    } catch {
+      print("エラーが発生しました！")
+    }
+    
+  }
+  
+  
 }
-
-
-
-
-
 
 
 
